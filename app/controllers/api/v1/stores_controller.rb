f6c79d7189cd86df8009e020f79304ea7d60e977
Bruno Tostes <btfjulio@hotmail.com>
2020-11-08 17:36:52 -0300

    add query to stores index

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,7 +1,20 @@
+require 'pry'
+
 module Api::V1
   class StoresController < ApplicationController
     
     before_action :set_store, only: %i(show)
+
+    def index 
+    
+      
+      @stores = Store.joins(:ratings)
+                    .select('stores.*, AVG(ratings.value) as a')
+                    .within(params[:longitude].to_f, params[:latitude].to_f, 5000)
+                    .group('stores.id')
+                    .order('a DESC')
+    end
+
 
     def show;end
     
